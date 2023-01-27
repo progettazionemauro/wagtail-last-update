8a54d9489f895c49a056f33bf3964d534f3194d1
progettazionemauro <progettazione@studiogb.org>
2023-01-27 10:04:35 +0100

    primo

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!2k-0by$k6ooox_d8bp)tz%*vk8qxq80-fag$vtbh5^6gmrkbz"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
